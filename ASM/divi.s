2401cb24d9da3484e47d66623b0c9d354d90454a
Edwige Cyffers <edwige.cyffers@ens-lyon.fr>
2017-11-09 18:19:16 +0100

    division qui prétend marcher

--- ASM/divi.s
+++ ASM/divi.s
@@ -1,22 +1,27 @@
-leti r0 17
-leti r1 5
-leti r2 0
-leti r3 5
+leti r0 17 
+leti r1 2 
+leti r2 0 
+let r3 r1 
 
+loopback:
 cmp r1 r0
-jumpif slt 22
+jumpif ge #loopend
 shift left r1 1
-jump -48
+jump #loopback
+loopend:
 
-
+boucle:
 cmp r3 r1
-jumpif slt 67
+jumpif ge #end
 shift right r1 1
 shift left r2 1
 cmp r0 r1
-jumpif ge 19
+jumpif lt #cond2
 	sub2 r0 r1
 	add2i r2 1
-jump -93
+cond2:
+jump #boucle
+end:
+shift right r2 1
 call -1
 
