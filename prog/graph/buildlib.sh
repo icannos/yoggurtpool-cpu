431daf084f096bdfc7f79a4791caa659f5f59e4e
Icannos <icannos.connect-radio@live.fr>
2017-12-22 22:23:48 +0100

    utils

--- prog/graph/buildlib.sh
+++ prog/graph/buildlib.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 # Build a library from a directory
-COMPILER_DIR = ../../compiler/includes_asm.py
+COMPILER_DIR = "../../compiler/includes_asm.py"
 python  ${COMPILER_DIR}  *.s  --prefix --exclusions $(ls -1 | sed -e 's/\..*$//') >  $(basename $(pwd)).sl
