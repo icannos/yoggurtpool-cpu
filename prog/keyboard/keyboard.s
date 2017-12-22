9252e7466abce9e37c90f15303068523371c3d01
Icannos <icannos.connect-radio@live.fr>
2017-12-22 16:26:00 +0100

    keyboard

--- prog/keyboard/keyboard.s
+++ prog/keyboard/keyboard.s
@@ -3,9 +3,23 @@
 #DEFINE KEYBOARD_BEGIN 0
 
 
-get_c:
+wait4key:
+
+
   leti r1 KEYBOARD_BEGIN
-  let  r1 r0
+
+
+  infboucle:
+    getctr a0 r2
+    sub2i r2 284
+    cmp r1 r2
+    jumpif slt next
+      setctr a0 r1
+    next:
+
+
+
+  jumpif z infboucle
 
   setctr a0 r1
   readze a0 1 r3
