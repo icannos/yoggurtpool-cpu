26e99c687a2598f53efb7cdd37ff3c85b7b8024a
Edwige Cyffers <edwige.cyffers@ens-lyon.fr>
2017-12-23 10:12:05 +0100

    fin de la structure globale de l'affichage

---
+++ tetris/nouvelle.s
@@ -0,0 +1,17 @@
+jump skipnouvelle
+nouvelle:
+
+
+;on cree les parametres aléatoires qui vont bien
+call -2
+let r3 r0
+call -2
+let r4
+call -2
+leti r1 100 ;on met la piece au milieu
+leti r2 127
+
+
+
+
+skipnouvelle:
