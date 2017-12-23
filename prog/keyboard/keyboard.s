a9aa51924645161acc314b2c54178e0b62caf558
Icannos <icannos.connect-radio@live.fr>
2017-12-23 04:32:33 +0100

    keyboard

--- prog/keyboard/keyboard.s
+++ prog/keyboard/keyboard.s
@@ -18,4 +18,29 @@ wait4key:
     readze a0 1 r0
     cmpi r0 0
 
-  jumpif n infboucle
+  jumpif nz infboucle
+
+  getctr a0 r0
+  sub2i r0 KEYBOARD_BEGIN
+  sub2i r0 1
+
+
+waitkey:
+  leti r1 KEYBOARD_BEGIN
+
+  infboucle:
+    getctr a0 r2
+    sub2i r2 284
+    cmp r1 r2
+
+    jumpif slt next
+    readze a0 1 r0
+
+    cmpi r0 0
+    jumpif z infboucle
+
+    next:
+
+    getctr a0 r0
+    sub2i r0 KEYBOARD_BEGIN
+    sub2i r0 1
