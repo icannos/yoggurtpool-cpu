9739ce67212fbb22a42adb903d8c8551e7a084c4
Edwige Cyffers <edwige.cyffers@ens-lyon.fr>
2017-12-27 17:49:03 +0100

    ça ressemble à un tetris !

--- tetris/tetris.s
+++ tetris/tetris.s
@@ -7,7 +7,7 @@
 #define JAUNE 65523
 #define VERT 992
 #define ROUGE 64512
-#DEFINE KEYBOARD_BEGIN 1073349796
+#define KEYBOARD_BEGIN 1073349796
 
 #include <graph.sl>
 #include <time.s>
@@ -83,15 +83,33 @@ descente:
 
 	cmpi r0 79 ;pour aller a droite
 	jumpif nz findroite
-	add2i r1 1
+	add2i r1 4
 	findroite:
 
 	cmpi r0 80 ; pour aller a gauche
 	jumpif nz fingauche
-	sub2i r1 1
+	sub2i r1 4
 	fingauche:
 
-	;cmpi r0 81 ;on tourne à droite
+	cmpi r0 81 ;on tourne à droite
+	jumpif nz fintour
+	cmpi r4 4
+	jumpif z vaut4
+	add2i r4 1
+	jump fintour
+	vaut4:
+	leti r4 0
+	fintour:
+
+	cmpi r0 82
+	jumpif nz fintourbis
+	cmpi r4 0
+	jumpif z vaut0
+	sub2i r4 1
+	jump fintourbis
+	vaut0:
+	leti r4 4
+	fintourbis:
 	
 
 ;79	0x04F	SDL_SCANCODE_RIGHT
